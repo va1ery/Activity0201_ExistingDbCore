3aed3ab1d1961d1c3a713f4cbb6a4737efb67b2c
Valery Gurov <va1ery@ya.ru>
2020-10-20 19:47:07 +0300

    Добавьте файлы проекта.

---
+++ Activity0201_ExistingDbCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Activity0201_ExistingDbCore
+{
+    class Program
+    {
+        static IConfigurationRoot _configuration;
+        static DbContextOptionsBuilder<AdventureWorks2012Context> _optionsBuilder;
+        static void Main(string[] args)
+        {
+            BuildConfiguration();
+            BuildOptions();
+            ListPeople();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true,
+            reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+        static void BuildOptions()
+        {
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorks2012Context>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks2012"));
+        }
+        static void ListPeople()
+        {
+            using (var db = new AdventureWorks2012Context(_optionsBuilder.Options))
+            {
+                var people = db.Person.OrderByDescending(x => x.LastName).Take(20).
+                ToList();
+                foreach (var person in people)
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
+        }
+    }
+}
---
+++ EF_Activity001/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Activity001
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
